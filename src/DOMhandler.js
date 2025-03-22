a7dbc37d22f182d31d20c52e615dde13c2ececa0
root <root@ZACK-PORTABLE.>
2025-03-22 15:52:23 -0400

    yessir

--- src/DOMhandler.js
+++ src/DOMhandler.js
@@ -1,11 +1,17 @@
 import "./css/sideBar.css"
+import "./img/"
 
 const DOMhandler = (function () {
 	const content = document.getElementById("content");
+	
+	
 	function showSideBar() {
 		const openSideBar = document.createElement("div");
-		openSideBar.classList.add("openSideBar")
-
+		openSideBar.classList.add("openSideBar");
+		
+		const openCloseButton = document.createElement("button");
+		const openCloseButtonImg = document.createElement("img");
+		openCloseButtonImg.src = "";
 		
 
 		content.appendChild(openSideBar)
